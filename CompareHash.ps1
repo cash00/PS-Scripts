6f1273cf6a984cfc1ca24afe62e11dad76663cec
cash00 <39612455+cash00@users.noreply.github.com>
2018-06-06 18:29:58 +0800

    Compare Hash
    Create a CSV file with the list of all the hashes and use this CSV file to compare

---
+++ CompareHash.ps1
@@ -0,0 +1,26 @@
+﻿Get-FileHash -Path C:\VisualStudioEnt2017\VisualStudioEnt2017.* -Algorithm SHA1 -Verbose|Export-Csv -NoTypeInformation -Path C:\VisualStudioEnt2017\list.csv -Verbose
+
+$listcsv = Import-Csv C:\VisualStudioEnt2017\list.csv
+
+foreach($list in $listcsv)
+{
+    $Algo = $list.Algorithm
+    $Hash = $list.Hash
+    $Fn = $list.Path
+    $Fn
+    Write-Host "Hash   :" $Hash
+
+    $Compare = (Get-FileHash -Path $Fn -Algorithm $Algo).Hash
+
+    Write-Host "Compare:" $Compare
+
+    if ($Compare -eq $Hash)
+    {
+    Write-Host "OK" -ForegroundColor Green
+    }
+    else
+    {
+    Write-Host "NOT OK" -ForegroundColor Red
+    }
+}
+
