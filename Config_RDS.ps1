723f09cab4e6e7c60ca1871b6ce953acf9afaecc
cash00 <39612455+cash00@users.noreply.github.com>
2019-01-11 10:24:13 +0800

    new

---
+++ Config_RDS.ps1
@@ -0,0 +1,112 @@
+﻿#$hostname = $env:computername
+$hostname = [System.Net.Dns]::GetHostByName(($env:computerName)).Hostname
+
+$data = @{
+AllNodes = @()
+
+RDSData = @{
+    ConnectionBroker = $hostname
+    SessionHost = $hostname
+    WebAccessServer = $hostname
+    CollectionName = 'zzzTEST'
+    AutomaticReconnectionEnabled = $true
+    DisconnectedSessionLimitMin = 360
+    IdleSessionLimitMin = 360
+    BrokenConnectionAction = 'Disconnect'
+    UserGroup = 'RDS Test'
+    LicenseServer = $hostname
+    LicenseMode = 'PerUser'
+}
+ 
+}
+
+#######################################################################################################
+
+$computername = [System.Net.Dns]::GetHostByName(($env:computerName)).Hostname
+
+Configuration RDS {
+ 
+param (
+)
+#region DSC Resource Modules
+#OBS!!! Be sure that the modules exist on the destination host servers
+ 
+Import-DscResource -ModuleName PSDesiredStateConfiguration, @{ModuleName='xRemoteDesktopSessionHost';ModuleVersion="1.8.0.0"}
+ 
+#endregion
+
+Node $computername {
+$RDData = $data.RDSData
+
+WindowsFeature RDSConnectionBroker {
+Name = 'RDS-Connection-Broker'
+Ensure = 'Present'
+}
+
+WindowsFeature SessionHost {
+Name = 'RDS-RD-Server'
+Ensure = 'Present'
+DependsOn = '[WindowsFeature]RDSConnectionBroker'
+}
+
+WindowsFeature WebAccess {
+Name = 'RDS-Web-Access'
+Ensure = 'Present'
+DependsOn = '[WindowsFeature]SessionHost'
+}
+
+WindowsFeature RDLicensing {
+Name = 'RDS-Licensing'
+Ensure = 'Present'
+DependsOn = '[WindowsFeature]WebAccess'
+}
+
+WindowsFeature RemoteDesktopServices {
+Name = 'Remote-Desktop-Services'
+Ensure = 'Present'
+}
+
+xRDSessionDeployment NewDeployment {
+ConnectionBroker = $RDData.ConnectionBroker
+SessionHost = $RDData.SessionHost
+WebAccessServer = $RDData.WebAccessServer
+DependsOn = '[WindowsFeature]RDLicensing'
+}
+
+xRDSessionCollection collection {
+CollectionName = $RDData.CollectionName
+SessionHost = $RDData.SessionHost
+ConnectionBroker = $RDData.ConnectionBroker
+DependsOn = '[xRDSessionDeployment]NewDeployment'
+}
+
+xRDSessionCollectionConfiguration collectionconfig {
+CollectionName = $RDData.CollectionName
+ConnectionBroker = $RDData.ConnectionBroker
+AutomaticReconnectionEnabled = $true
+DisconnectedSessionLimitMin = $RDData.DisconnectedSessionLimitMin
+IdleSessionLimitMin = $RDData.IdleSessionLimitMin
+BrokenConnectionAction = $RDData.BrokenConnectionAction
+UserGroup = $RDData.UserGroup
+DependsOn = '[xRDSessionCollection]collection'
+}
+
+xRDLicenseConfiguration licenseconfig {
+ConnectionBroker = $RDData.ConnectionBroker
+LicenseServer = $RDData.LicenseServer
+LicenseMode = $RDData.LicenseMode
+DependsOn = '[xRDSessionCollectionConfiguration]collectionconfig'
+}
+
+xRDRemoteApp zNotepad {
+CollectionName = $RDData.CollectionName
+Alias = 'zNotepad'
+DisplayName = 'zNotepad'
+FilePath = 'C:\Windows\notepad.exe'
+DependsOn = '[xRDLicenseConfiguration]licenseconfig'
+}
+}
+
+}
+
+RDS -OutputPath 'C:\Temp\RDSConfig\' -ConfigurationData $data -Verbose
---
+++ Set_LCM_RDS.ps1
@@ -0,0 +1,28 @@
+﻿#$hostname = $env:computername
+$hostname = [System.Net.Dns]::GetHostByName(($env:computerName)).Hostname
+
+[DscLocalConfigurationManager()]
+
+configuration LCM {
+ 
+param ( 
+[parameter(Mandatory=$true)]
+[string[]]$computername
+)
+
+node $computername {
+
+settings {
+    ActionAfterReboot = 'ContinueConfiguration' #Change to default 'ContinueConfiguration' is other value
+    ConfigurationMode = 'ApplyAndMonitor' #Change back to default 'ApplyAndMonitor' after completed
+    RebootNodeIfNeeded = $true #Change back to default $False after completed
+}
+}
+}
+
+$computername = $env:computername
+
+LCM -OutputPath 'C:\Temp\LCMSettings\' -computername $computername -verbose
+Set-DscLocalConfigurationManager -Path 'C:\Temp\LCMSettings\' -ComputerName $computername -Verbose -Force
+
+Get-DscLocalConfigurationManager|fl
