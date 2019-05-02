47610491d8f346d9961c7eb92736d5459614fb2e
cash00 <39612455+cash00@users.noreply.github.com>
2019-05-02 18:28:41 +0800

    Update ZIPFile.ps1

--- ZIPFile.ps1
+++ ZIPFile.ps1
@@ -6,3 +6,12 @@ $Source = "C:\test"
 $Destination = "C:\TEMP\AAAAAAAAAAAAAAAA.zip"
 
 [System.IO.Compression.ZipFile]::CreateFromDirectory($Source,$Destination,$Compression,$IncludeBaseDirectory)
+
+
+Compress-Archive -LiteralPath 'C:\test' -DestinationPath "C:\TEMP\AAAAAAAAAAAAAAAA.zip" -CompressionLevel Optimal
+
+Expand-Archive -LiteralPath "C:\TEMP\AAAAAAAAAAAAAAAA.zip" -DestinationPath "C:\" -Force
+
+
+
+
