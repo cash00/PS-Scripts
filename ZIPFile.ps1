df9f6e83439078061780c7efcd9eb3888381cd97
cash00 <39612455+cash00@users.noreply.github.com>
2019-05-02 17:39:56 +0800

    Create ZIPFile.ps1

---
+++ ZIPFile.ps1
@@ -0,0 +1,8 @@
+﻿[Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+$Compression = [System.IO.Compression.CompressionLevel]::Optimal
+$IncludeBaseDirectory = $false
+
+$Source = "C:\test"
+$Destination = "C:\TEMP\AAAAAAAAAAAAAAAA.zip"
+
+[System.IO.Compression.ZipFile]::CreateFromDirectory($Source,$Destination,$Compression,$IncludeBaseDirectory)
