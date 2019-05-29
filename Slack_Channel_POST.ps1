426ce09446ae4afe0e1398f528dda5308c3b0bdf
cash00 <39612455+cash00@users.noreply.github.com>
2019-05-29 19:34:26 +0800

    Slack Channel POST
    Add Slack Channel POST

---
+++ Slack_Channel_POST.ps1
@@ -0,0 +1,10 @@
+﻿
+$slackuri = 'https://hooks.slack.com/services/'
+$ChannelID = '' #right click channel
+
+$data = Get-Content -Path C:\Users\admin\Desktop\Slack_JSON_test.JSON
+
+$contype = 'application/json'
+$meth = 'Post'
+
+Invoke-RestMethod -Uri $slackuri -Body $data -ContentType $contype -Method $meth
