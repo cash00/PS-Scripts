9f9449abfdb911500b9c41ac34d688fb6f4c3adf
cash00 <39612455+cash00@users.noreply.github.com>
2019-05-29 20:10:43 +0800

    Update Slack_Channel_POST.ps1

--- Slack_Channel_POST.ps1
+++ Slack_Channel_POST.ps1
@@ -1,8 +1,8 @@
 ﻿
 $slackuri = 'https://hooks.slack.com/services/'
 $ChannelID = '' #right click channel
-
-$data = Get-Content -Path .\Slack_JSON_test.JSON
+$FN = $MyInvocation.MyCommand.Path.TrimEnd("Slack_Channel_POST.ps1")+'\Slack_JSON_test.JSON'
+$data = Get-Content -Path $FN
 
 $contype = 'application/json'
 $meth = 'Post'
